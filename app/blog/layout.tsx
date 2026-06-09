4e182c4c09a35bd1ca095498e81287f17a7dae7d
brent-prog <brent@3psolutions.ca>
2026-06-09 00:00:11 -0400

    Add article CTA to blog article pages

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import type { ReactNode } from "react";
+import { usePathname } from "next/navigation";
 import SiteHeader from "../components/SiteHeader";
+import CallToAction from "../components/CallToAction";
 
 export default function BlogLayout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  const showArticleCTA = pathname !== "/blog";
+
   return (
     <>
       <SiteHeader />
       {children}
+      {showArticleCTA && (
+        <div className="bg-slate-50 px-5 pb-20 lg:px-8">
+          <div className="mx-auto max-w-4xl">
+            <CallToAction />
+          </div>
+        </div>
+      )}
     </>
   );
 }
